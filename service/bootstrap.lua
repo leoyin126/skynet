0e141df6bebdbb92e86e3acddbd1719949a3f39f
leoyin126 <125019097@qq.com>
2020-05-17 17:05:07 +0800

    save

--- service/bootstrap.lua
+++ service/bootstrap.lua
@@ -9,11 +9,13 @@ skynet.start(function()
 	skynet.name(".launcher", launcher)
 
 	local harbor_id = tonumber(skynet.getenv "harbor" or 0)
+	-- master 通过 harbor 是否配置为 0 来判断是否一个单节点 skynet 网络。
 	if harbor_id == 0 then
 		assert(standalone ==  nil)
 		standalone = true
 		skynet.setenv("standalone", "true")
-
+		-- 单节点模式下，是不需要节点间通讯的。
+		-- 启动一个叫做 cdummy 的服务，它负责拦截对外广播的全局名字变更。
 		local ok, slave = pcall(skynet.newservice, "cdummy")
 		if not ok then
 			skynet.abort()
@@ -35,6 +37,8 @@ skynet.start(function()
 	end
 
 	if standalone then
+		-- datacenter 可用来在整个 skynet 网络做跨节点的数据共享。
+		-- datacenter.set(key1, key2, ... , value)
 		local datacenter = skynet.newservice "datacenterd"
 		skynet.name("DATACENTER", datacenter)
 	end
